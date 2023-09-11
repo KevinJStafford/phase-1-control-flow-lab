0741bcad3c4b2cec5d5c54252411944afb58abfd
Kevin <staffordkevinjames@gmail.com>
2023-09-11 15:30:46 -0400

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let message
+  if (feet <= 400) {
+    message = `This one is on me!`;
+  } else if (feet < 2000) {
+    message = 'That will be twenty bucks.';
+  } else if (feet <= 2500) {
+    message = `I will gladly take your thirty bucks.`;
+  } else if (feet > 2500) {
+    message = `No can do.`;
+  }
+  return message;
+};
+
+function ternaryCheckCity(city){
+  let cityMessage = city == 'NYC' ? "Ok, sounds good." : "No go.";
+
+  return cityMessage;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipAmount){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  let charmResponse;
+
+  switch (tipAmount) {
+    case 'generous':
+      charmResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charmResponse = 'Thank you.'
+      break;
+    default:
+      charmResponse = "Bye.";
+      break;
+  };
+  return charmResponse;
+};
